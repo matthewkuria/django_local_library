994181ba3330e510c6d1793813abd2c3c6f2301f
matthewkuria <mathewkwachira@gmail.com>
2024-07-03 22:03:39 +0300

    Add home page to the app

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, Author, BookInstance, Genre
 
 def index(request):
-    return HttpResponse("Hello Matthew")
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_react_books = Book.objects.filter(title__exact='React JS').count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+    # Generate the number of genres
+    num_genre = Genre.objects.all().count()
+    num_genre_instances = Genre.objects.filter(name__exact='Fantasy').count()
+
+    context = {
+        'num_books': num_books,
+        'num_react_books':num_react_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genre': num_genre,
+        'num_genre_instances': num_genre_instances,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
